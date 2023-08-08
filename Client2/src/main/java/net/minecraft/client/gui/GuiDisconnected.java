bd17eb76ab7c0cd630faebab459cfa17ca646a3a
vlouboos <vlouyearlinjinhua@outlook.com>
2023-08-08 17:30:55 +0800

    喜报

--- Client2/src/main/java/net/minecraft/client/gui/GuiDisconnected.java
+++ Client2/src/main/java/net/minecraft/client/gui/GuiDisconnected.java
@@ -2,19 +2,21 @@ package net.minecraft.client.gui;
 
 import java.io.IOException;
 import java.util.List;
+
+import cn.floatingpoint.min.utils.render.RenderUtil;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
-public class GuiDisconnected extends GuiScreen
-{
+public class GuiDisconnected extends GuiScreen {
     private final String reason;
     private final ITextComponent message;
     private List<String> multilineMessage;
     private final GuiScreen parentScreen;
     private int textHeight;
+    private boolean banned;
 
-    public GuiDisconnected(GuiScreen screen, String reasonLocalizationKey, ITextComponent chatComp)
-    {
+    public GuiDisconnected(GuiScreen screen, String reasonLocalizationKey, ITextComponent chatComp) {
         this.parentScreen = screen;
         this.reason = I18n.format(reasonLocalizationKey);
         this.message = chatComp;
@@ -24,29 +26,29 @@ public class GuiDisconnected extends GuiScreen
      * Fired when a key is typed (except F11 which toggles full screen). This is the equivalent of
      * KeyListener.keyTyped(KeyEvent e). Args : character (character on the key), keyCode (lwjgl Keyboard key code)
      */
-    protected void keyTyped(char typedChar, int keyCode) throws IOException
-    {
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
     }
 
     /**
      * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
      * window resizes, the buttonList is cleared beforehand.
      */
-    public void initGui()
-    {
+    public void initGui() {
         this.buttonList.clear();
         this.multilineMessage = this.fontRenderer.listFormattedStringToWidth(this.message.getFormattedText(), this.width - 50);
         this.textHeight = this.multilineMessage.size() * this.fontRenderer.FONT_HEIGHT;
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, Math.min(this.height / 2 + this.textHeight / 2 + this.fontRenderer.FONT_HEIGHT, this.height - 30), I18n.format("gui.toMenu")));
+        for (String s : this.multilineMessage) {
+            if (banned) return;
+            this.banned = s.toLowerCase().contains(" ban ") || s.toLowerCase().contains(" banned ") || s.toLowerCase().contains("封禁");
+        }
     }
 
     /**
      * Called by the controls from the buttonList when activated. (Mouse pressed for buttons)
      */
-    protected void actionPerformed(GuiButton button) throws IOException
-    {
-        if (button.id == 0)
-        {
+    protected void actionPerformed(GuiButton button) throws IOException {
+        if (button.id == 0) {
             this.mc.displayGuiScreen(this.parentScreen);
         }
     }
@@ -54,16 +56,17 @@ public class GuiDisconnected extends GuiScreen
     /**
      * Draws the screen and all the components in it.
      */
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
-        this.drawDefaultBackground();
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        if (!this.banned) {
+            this.drawDefaultBackground();
+        } else {
+            RenderUtil.drawImage(new ResourceLocation("client/guis/disconnected/good_news.png"), 0, 0, this.width, this.height);
+        }
         this.drawCenteredString(this.fontRenderer, this.reason, this.width / 2, this.height / 2 - this.textHeight / 2 - this.fontRenderer.FONT_HEIGHT * 2, 11184810);
         int i = this.height / 2 - this.textHeight / 2;
 
-        if (this.multilineMessage != null)
-        {
-            for (String s : this.multilineMessage)
-            {
+        if (this.multilineMessage != null) {
+            for (String s : this.multilineMessage) {
                 this.drawCenteredString(this.fontRenderer, s, this.width / 2, i, 16777215);
                 i += this.fontRenderer.FONT_HEIGHT;
             }
