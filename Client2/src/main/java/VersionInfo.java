90c12097fddb460521d8a27c54b84c644d86a117
vlouboos <vlouyearlinjinhua@outlook.com>
2023-08-13 16:05:18 +0800

    Add VersionInfo

---
+++ Client2/src/main/java/VersionInfo.java
@@ -0,0 +1,13 @@
+import cn.floatingpoint.min.MIN;
+
+/**
+ * @projectName: MIN
+ * @author: vlouboos
+ * @date: 2023-07-29 14:51:29
+ */
+@SuppressWarnings("unused")
+public class VersionInfo {
+    public static String getVersion() {
+        return MIN.VERSION;
+    }
+}
